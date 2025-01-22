c6852447db083525acd80c0ed6384e7736cf6292
Johan Thomsen <write@ownrisk.dk>
2025-01-22 17:54:59 +0100

    flake/nixos: 24.05 -> 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "concealed";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     crane.url = "github:ipetkov/crane";
   };
 
